cda0f9ace37326339f0794b43d39eb368aaaed7b
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-03-09 23:21:23 +0900

    using 2D context
    canvas에 그림을 그리기 위해 context를 사용한다.

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-// cavnas위에 마우스가 있으면 인식하도록
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext('2d');
+
+// context의 default 설정 (검정색, 선넓이 = 2.5)
+ctx.strokeStyle = "#2c2c2c"; 
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
+
 function stopPainting() {
-    painting = flase;
+    painting = false;
+}
+
+
+function startPainting() {
+    painting = true;
 }
 
 
@@ -13,26 +23,20 @@ function onMouseMove(event) {
     // 윈도우 전체 범위가 아닌 canvas내 좌표를 사용하기 위해 offset 사용
     const x = event.offsetX;
     const y = event.offsetY;
-
-}
-
-
-// 마우스 클릭버튼을 누르거나 유지할 때 발생하는 이벤트를 수행
-function onMouseDown(event) {
-    painting = true;
-}
-
-
-// 마우스 클릭버튼을 손에서 뗄 때 발생하는 이벤트를 수행
-function onMouseUp(event) {
-    stopPainting();
+    if (!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
 
 // canvas가 존재하는지 확인
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
